9aca64bc2f64bdd1830037ca65f8fdb79fa45995
Marta M. Álvarez Crespo <marta.maria.alvarez.crespo@udc.es>
2024-05-05 19:45:20 +0200

    deleted:    prueba1.py

--- prueba1.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import RocCurveDisplay, ConfusionMatrixDisplay, classification_report
-import numpy as np
-
-y_true= ["Marta", "Marta", "Marta", "Marta"]
-y_pred= ["Marta", "Marta", "Marta", "Maria"]
-
-print(classification_report(y_true, y_pred, zero_division= np.nan))
