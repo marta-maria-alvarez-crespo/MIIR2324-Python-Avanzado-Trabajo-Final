6e23156e6e579b420da7b89f9dc58b117448f951
Marta M. Álvarez Crespo <145216674+marta-maria-alvarez-crespo@users.noreply.github.com>
2024-05-24 00:23:21 +0200

    Pruebas multiproceso

--- mi_hilo.py
+++ mi_hilo.py
@@ -1,6 +1,11 @@
 from threading import Thread
-from main import configuracion
+from multiprocessing import Process
+from concurrent.futures import ThreadPoolExecutor
 
+import pandas as pd
+
+def f(X, Y):
+    return pd.DataFrame({"a": [X, X, X], "b": [Y, Y, Y]}), "hola"
 
 class MiHilo(Thread):
     def __init__(self, target, args):
@@ -11,3 +16,57 @@ class MiHilo(Thread):
 
     def get_result(self):
         return self.result
+
+class MiProceso(Process):
+    def __init__(self, target, args, queue):
+        super().__init__(target=target, args=args)
+        self.queue = queue
+
+    def run(self):
+        result = self._target(*self._args)
+        self.queue.put(result)
+
+def m():
+    hilos = []
+    for i in range(4):
+        hilo = MiHilo(target=f, args=([i]))
+        hilo.start()
+        hilos.append(hilo)
+
+    for hilo in hilos:
+        hilo.join()
+
+    df = pd.DataFrame()
+    for hilo in hilos:
+        result, hola = hilo.get_result()
+        df = pd.concat([df, pd.DataFrame(result)], join= "outer", axis= 0, ignore_index= True)
+    print(df)
+
+def m2():
+    neuronas = [1,2,3,4]
+    capas = [5, 6, 7, 8]
+    futures = []
+    with ThreadPoolExecutor(max_workers=4) as pool:
+        for n in neuronas:
+            for c in capas:
+                futures.append(pool.submit(f, n, c))
+        results = [f.result() for f in futures]
+
+    df = pd.DataFrame()
+    for result in results:
+        df = pd.concat([df, pd.DataFrame(result[0])], join= "outer", axis= 0, ignore_index= True)
+        print(result[1])
+    print(df)
+
+def m3():
+    neuronas = [1,2,3,4]
+    with ThreadPoolExecutor(max_workers=4) as pool:
+        results = list(pool.map(f, neuronas, [i+1 for i in neuronas]))
+
+    df = pd.DataFrame()
+    for result in results:
+        df = pd.concat([df, pd.DataFrame(result)], join= "outer", axis= 0, ignore_index= True)
+    print(df)
+
+if __name__ == "__main__":
+    m2()
